545a0f64d043870b1973c644609dda4b64781e8c
giyos <rashidovgiyos409@.gmail.com>
2023-11-02 18:54:29 -0300

    fix route dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { logo2 } from "./Assets/images";
 import { Navbar, Footer, Main, Modal } from "./components";
 import {
   Service,
@@ -12,6 +13,7 @@ import {
 } from "./Pages";
 import { Route, Routes } from "react-router-dom";
 import { Context } from "./Context/ModalContext";
+import Addrequest from "./Pages/Addrequest";
 
 const App = () => {
   const { modalShow, setModalShow } = useContext(Context);
@@ -22,7 +24,7 @@ const App = () => {
     <div className="nunito bg-[#F3F9FF]">
       {/* <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" index element={<Main />} />
         <Route path="/service" element={<Service />} />
         <Route path="/loadingProducts" element={<LoadingProducts />} />
         <Route path="/loadingProducts2" element={<LoadingProductsTwo />} />
@@ -32,7 +34,30 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer /> */}
-      <Dashboard />
+
+       <nav className="py-[20px] bg-white border-b border-[#F3F9FF]">
+        <div className="px-[30px]">
+          <div className="flex items-center justify-between">
+            <div>
+              <img src={logo2} alt="" />
+            </div>
+            <div className="flex items-center gap-[30px]">
+              <p className="text-lg font-bold cursor-pointer">Заявки</p>
+              <p className="text-lg font-bold cursor-pointer">Настройки</p>
+            </div>
+            <div>
+              <button className="text-lg font-bold text-[#FF6969]">
+                Выйти
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path='/' index element={<Dashboard />}/>
+        <Route path='/add' index element={< Addrequest/>}/>
+      </Routes>
+      
       {modalShow && <Modal />}
     </div>
   );
